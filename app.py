78fab74a175c9c5bc9bcf0c087f2a4368b1d6280
jrathod9 <lewisburnard@gmail.com>
2019-06-06 17:47:32 +0530

    data extraction finishes

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
 from flask import Flask,render_template,request,redirect,url_for
-
+from InstagramAPI import InstagramAPI
+import time
 
 app = Flask(__name__)
 app.secret_key= '04c816ee38ea4ee1843c22a6e67d8406'
+
+
+def getFollowers(InstagramAPI,user_id):
+	followers = []
+	InstagramAPI.getProfileData()
+	user_id= InstagramAPI.LastJson['user']['pk']
+	next_max_id = True
+	while next_max_id:
+	    if next_max_id == True:
+	            next_max_id = ''
+	    _ = InstagramAPI.getUserFollowers(user_id,maxid = next_max_id)
+	    followers.extend(InstagramAPI.LastJson.get('users',[]))
+	    next_max_id = InstagramAPI.LastJson.get('next_max_id','')
+	    time.sleep(1)
+	return followers
+
+def getFollowing(InstagramAPI,user_id):
+	following = []
+	InstagramAPI.getProfileData()
+	user_id= InstagramAPI.LastJson['user']['pk']
+	next_max_id = True
+	while next_max_id:
+	    if next_max_id == True:
+	            next_max_id = ''
+	    _ = InstagramAPI.getUserFollowings(user_id,maxid = next_max_id)
+	    following.extend(InstagramAPI.LastJson.get('users',[]))
+	    next_max_id = InstagramAPI.LastJson.get('next_max_id','')
+	    time.sleep(1)
+	return following
 
 @app.route('/', methods = ['GET','POST'])
 def home():	
@@ -13,29 +43,30 @@ def profile():
 	if request.method == 'POST':
 		username = request.form["username"]
 		password = request.form["password"]
+
 		from InstagramAPI import InstagramAPI
-		import time
 		InstagramAPI = InstagramAPI(username, str(password))
 		InstagramAPI.login()
 		InstagramAPI.getProfileData()
 		user_id= InstagramAPI.LastJson['user']['pk']
+
+		me = [ InstagramAPI.LastJson['user']['username'] ,
+		 		InstagramAPI.LastJson['user']['full_name'] ,
+		 		InstagramAPI.LastJson['user']['profile_pic_url'] ,
+		 		InstagramAPI.LastJson['user']['biography'] ]
+		print(me)
 		import time
 		followers = []
-		next_max_id = True
-		while next_max_id:
-		    print next_max_id
-		    if next_max_id == True:
-		            next_max_id = ''
-		    _ = InstagramAPI.getUserFollowers(user_id,maxid = next_max_id)
-		    followers.extend(InstagramAPI.LastJson.get('users',[]))
-		    next_max_id = InstagramAPI.LastJson.get('next_max_id','')
-		    time.sleep(1)
-		followers_list = followers
-		print(len(followers_list))
-	return render_template('profile.html')
+		following = []
+		followers_list = getFollowers(InstagramAPI,user_id)
+		following_list = getFollowing(InstagramAPI,user_id)
+		followers_list_usernames = [ele['username'] for ele in followers_list]
+		following_list_usernames = [ele['username'] for ele in following_list]
 
+		fans = set(followers_list_usernames).difference(set(following_list_usernames))
+		celebs = set(following_list_usernames).difference(set(followers_list_usernames))
 
-
+	return render_template('profile.html' , me = me ,followers = followers , following = following , fans = fans , celebs = celebs)
 
 if __name__ == '__main__':
     app.run(port=5000,debug = True)
